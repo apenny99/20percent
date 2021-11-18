cb997d25f31361260c8167253662af15adde34ca
apenny23 <apenny23@kentdenver.org>
2021-11-18 10:33:29 -0700

    Added instance data and accessor methods to the dataNode class

--- dataNode.py
+++ dataNode.py
@@ -1,12 +1,45 @@
+import smbus2
+import bme280
+
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+
+calibration_params = bme280.load_calibration_params(bus, address)
+# the sample method will take a single reading and return a
+# compensated_reading object
+data = bme280.sample(bus, address, calibration_params)
+
+
+# "Instance Data"
+timestamp = data.timestamp
+temprature = data.temprature
+pressure = data.pressure
+humidity = data.humidity
 
 
 
-timestamp = "blank"
-temprature = 0.0
-pressure = 0.0
-humidity = 0
+# def newNode(time, temp, pres, humid):
+#     timestamp=time
+#     temprature=temp
+#     pressure=pres
+#     humidity=humid
 
 
+# returns the timestamp
+def returnTime():
+    return timestamp
 
-def newNode(time, temp, pressure, humidity):
-    
+# returns the temprature
+def returnTemp():
+    return temprature
+
+# returns the pressure
+def returnPres():
+    return pressure
+
+# returns the humidity
+def returnHumidity():
+    return humidity
+
+
